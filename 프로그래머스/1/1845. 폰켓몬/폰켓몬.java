a6e941e6fd9274bde9a2f26164af3630b75b5ae6
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2024-05-14 08:59:21 +0900

    [level 1] Title: 폰켓몬, Time: 12.44 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int len = nums.length / 2;
+
+        return Math.min(set.size(), len);
+    }
+}
