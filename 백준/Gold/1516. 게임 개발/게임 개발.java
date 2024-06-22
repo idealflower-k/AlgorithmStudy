9561d4aa79e0280b1f6d9de321a1ec0fbd9e1161
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2024-06-22 11:03:31 +0900

    [Gold III] Title: 게임 개발, Time: 276 ms, Memory: 21912 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,28 +1,24 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static int[] resultArray;
 	private static int[][] dataArray;
-	// private static ArrayList<Integer>[] datas;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
 		int testCase = Integer.parseInt(br.readLine());
 		resultArray = new int[testCase + 1];
-		// datas = new ArrayList[testCase + 1];
 		dataArray = new int[testCase + 1][500];
 
-		// for (int i = 1; i < testCase + 1; i++) {
-		// 	datas[i] = new ArrayList<>();
-		// }
-
-		for (int i = 0; i < testCase; i++) {
+		for (int i = 0; i < testCase; ++i) {
 			st = new StringTokenizer(br.readLine());
 			dataArray[i + 1][0] = Integer.parseInt(st.nextToken());
 			String input = st.nextToken();
@@ -32,12 +28,12 @@ public class Main {
 				input = st.nextToken();
 			}
 		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i < testCase + 1; i++) {
-			sb.append(dp(i)).append("\n");
+		for (int i = 1; i < testCase + 1; ++i) {
+			bw.write(dp(i) + "\n");
 		}
-
-		System.out.println(sb);
+		bw.flush();
+		// bw.close();
+		// br.close();
 	}
 
 	static int dp(int n) {
@@ -45,15 +41,11 @@ public class Main {
 			return resultArray[n];
 		}
 
-		// ArrayList<Integer> integers = datas[n];
 		int[] ints = dataArray[n];
 		int semi = 0;
 		for (int i = 1; ints[i] != 0; i++) {
 			semi = Math.max(dp(ints[i]), semi);
 		}
-		// for (int i : integers) {
-		// 	semi = Math.max(dp(i), semi);
-		// }
 		return resultArray[n] = semi + dataArray[n][0];
 	}
 }
