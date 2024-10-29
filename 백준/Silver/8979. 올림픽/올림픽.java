68c179efdb20db588c699c06259ea59ec366213d
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2024-10-29 22:34:01 +0900

    [Silver V] Title: 올림픽, Time: 132 ms, Memory: 14680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,8 +1,19 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
+// TODO: 가중치는 안됨, class를 구현하고 람다식으로 정렬
 public class Main {
+	public static class node {
+		int id, gold, silver, brown;
+
+		public node(int id, int gold, int silver, int brown) {
+			this.id = id;
+			this.gold = gold;
+			this.silver = silver;
+			this.brown = brown;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer token = new StringTokenizer(buffer.readLine());
@@ -10,25 +21,42 @@ public class Main {
 		int numCountry = Integer.parseInt(token.nextToken());
 		int target = Integer.parseInt(token.nextToken());
 
-		double[] value = new double[numCountry + 1];
+		node[] value = new node[numCountry];
 
 		for (int i = 0; i < numCountry; i++) {
 			token = new StringTokenizer(buffer.readLine());
 			int num = Integer.parseInt(token.nextToken());
 			int gold = Integer.parseInt(token.nextToken());
 			int silver = Integer.parseInt(token.nextToken());
-			int iron = Integer.parseInt(token.nextToken());
+			int brown = Integer.parseInt(token.nextToken());
 
-			value[num] = (gold * 3) + (silver * 0.0000002) + (iron * 0.0000000000001);
+			value[i] = new node(num, gold, silver, brown);
 		}
 
-		double targetValue = value[target];
+		Arrays.sort(value, (a, b) -> {
+			if (a.gold != b.gold) {
+				return b.gold - a.gold;
+			}
+			if (a.silver != b.silver) {
+				return b.silver - a.silver;
+			}
+			return b.brown - a.brown;
+		});
+
 		int result = 1;
-		for (int i = 1; i < numCountry; i++) {
-			if (targetValue < value[i]) {
-				result++;
+
+		for (int i = 0; i < numCountry; i++) {
+			if (i > 0 && !isSameMedal(value[i], value[i - 1])) {
+				result = i + 1;
+			}
+			if (value[i].id == target) {
+				System.out.println(result);
+				break;
 			}
 		}
-		System.out.println(result);
+	}
+
+	public static boolean isSameMedal(node a, node b) {
+		return a.gold == b.gold && a.silver == b.silver && a.brown == b.brown;
 	}
 }
