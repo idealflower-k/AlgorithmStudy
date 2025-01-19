a22abfd1d300df7376708e14cc65d6cbcfde8e64
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2025-01-20 01:13:03 +0900

    [Silver II] Title: N과 M (9), Time: 164 ms, Memory: 20188 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int N;
+    static int M;
+    static int[] arr;
+    static int[] list;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+        arr = new int[N];
+        list = new int[M];
+        visited = new boolean[N];
+
+        token = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; ++i) {
+            arr[i] = Integer.parseInt(token.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        dfs(0);
+        
+        System.out.print(sb);
+    }
+    
+    private static void dfs(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; ++i) {
+                sb.append(list[i]);
+                if (i != M - 1) {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        int lastUsed = -1;
+        for (int i = 0; i < N; ++i) {
+            if (!visited[i] && arr[i] != lastUsed) {
+                visited[i] = true;
+                list[cnt] = arr[i];
+                lastUsed = arr[i];
+                dfs(cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
