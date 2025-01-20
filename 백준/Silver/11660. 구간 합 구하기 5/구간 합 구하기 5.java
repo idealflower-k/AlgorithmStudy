932feaeba0912d2d777472b33baaa205f975096e
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2025-01-20 15:47:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 808 ms, Memory: 122420 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     
     static int[][] board;
-    static int[][] dp;
+    static int[][] prefixSum;
     static StringBuilder sb = new StringBuilder();
     static int N;
     
@@ -15,13 +15,13 @@ public class Main {
         N = Integer.parseInt(token.nextToken());
         int T = Integer.parseInt(token.nextToken());
         board = new int[N + 1][N + 1];
-        dp = new int[N + 1][N + 1];
+        prefixSum = new int[N + 1][N + 1];
         
         for (int i = 1; i <= N; ++i) {
             token = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; ++j) {
                 board[i][j] = Integer.parseInt(token.nextToken());
-                dp[i][j] = dp[i][j - 1] + board[i][j];
+                prefixSum[i][j] = prefixSum[i][j - 1] + prefixSum[i - 1][j] - prefixSum[i - 1][j - 1] + board[i][j];
             }
         }
         
@@ -32,10 +32,7 @@ public class Main {
             int x2 = Integer.parseInt(token.nextToken());
             int y2 = Integer.parseInt(token.nextToken());
             
-            int res = 0;
-            for (int j = x1; j <= x2; ++j) {
-                res += dp[j][y2] - dp[j][y1 - 1];
-            }
+            int res = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
             sb.append(res).append("\n");
         }
         
