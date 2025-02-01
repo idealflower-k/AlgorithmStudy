d1f93afe38ad1356e2e328167a32530fc2c65982
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2025-02-02 00:32:14 +0900

    [Gold III] Title: 미로 탈출하기, Time: 1764 ms, Memory: 48624 KB -BaekjoonHub

---
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int N;
+    static int M;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[][] dp;
+    static int[] U = {-1, 0};
+    static int[] R = {0, 1};
+    static int[] D = {1, 0};
+    static int[] L = {0, -1};
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+        
+        map = new char[N][M];
+        dp = new int[N][M];
+        visited = new boolean[N][M];
+        
+        for (int i = 0; i < N; ++i) {
+            String line = br.readLine();
+            map[i] = line.toCharArray();
+        }
+        
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < M; ++j) {
+                if (dp[i][j] == -1) {
+                    for (boolean[] arr : visited) {
+                        Arrays.fill(arr, false);
+                    }
+                    dp[i][j] = dfs(i, j);
+                }
+            }
+        }
+        
+        int result = 0;
+        for (int[] arr : dp) {
+            for (int i : arr) {
+                if (i == 1) {
+                    result++;
+                }
+            }
+        }
+        System.out.print(result);
+    }
+    
+    private static int dfs(int r, int c) {
+        if (dp[r][c] != -1) {
+            return dp[r][c];
+        }
+        
+        visited[r][c] = true;
+        
+        char curr = map[r][c];
+        int nr = r;
+        int nc = c;
+        if (curr == 'U') {
+            nr = r + U[0];
+            nc = c + U[1];
+        } else if (curr == 'D') {
+            nr = r + D[0];
+            nc = c + D[1];
+        } else if (curr == 'R') {
+            nr = r + R[0];
+            nc = c + R[1];
+        } else if (curr == 'L') {
+            nr = r + L[0];
+            nc = c + L[1];
+        }
+        
+        if (isOut(nr, nc)) {
+            dp[r][c] = 1;
+            return 1;
+        } else {
+            dp[r][c] = 0;
+            if (!visited[nr][nc]) {
+                dp[r][c] = dfs(nr, nc);
+            }
+        }
+        return dp[r][c];
+    }
+    
+    private static boolean isOut(int r, int c) {
+        return r < 0 || c < 0 || r >= N || c >= M;
+    }
+}
